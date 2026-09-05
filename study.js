1f3d93d10c542e0ee43b31e5737adae6166ee8a8
Blaze-73 <202257192+Blaze-73@users.noreply.github.com>
2026-09-05 23:44:21 +0100

    lesson 2: functions

--- study.js
+++ study.js
@@ -3,3 +3,8 @@ const name = 'Mouataz';
 let age = 20;
 var city = 'Algiers';
 console.log(name, age, city);
+
+
+// Lesson 2: Functions
+function greet(n) { return 'Hello ' + n; }
+const add = (a, b) => a + b;
