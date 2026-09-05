8ec11ea73c08d435293828258fd6442684d8b84f
Blaze-73 <202257192+Blaze-73@users.noreply.github.com>
2026-09-05 23:44:22 +0100

    lesson 3: arrays

--- study.js
+++ study.js
@@ -8,3 +8,9 @@ console.log(name, age, city);
 // Lesson 2: Functions
 function greet(n) { return 'Hello ' + n; }
 const add = (a, b) => a + b;
+
+
+// Lesson 3: Arrays
+const langs = ['js', 'python', 'go'];
+langs.push('rust');
+console.log(langs.map(l => l.toUpperCase()));
